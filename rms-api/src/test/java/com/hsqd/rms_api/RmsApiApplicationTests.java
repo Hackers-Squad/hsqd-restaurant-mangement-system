bca0d2ba492fc450d8a0575841db3f9b14f9cabd
Thathsara Dasun Wickramasooriya <32405325+dasunwickr@users.noreply.github.com>
2024-07-21 13:03:05 +0530

    gitignore setup

---
+++ rms-api/src/test/java/com/hsqd/rms_api/RmsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsqd.rms_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
